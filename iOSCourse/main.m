cad9d9fb23fad1d51c2220adcc68220adc8d9117
Alex Basson <alexbasson@gmail.com>
2014-04-19 20:42:56 -0400

    Create empty application project

---
+++ iOSCourse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSCourse
+//
+//  Created by Alex on 4/19/14.
+//  Copyright (c) 2014 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
